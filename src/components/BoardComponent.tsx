544f238ecb15178cebfbdfcf8e1e59e8a5856327
stepGT <stepGT@mail.ru>
2022-05-28 22:51:26 +1000

    Added model highlightCells and getCopyBoard

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Board } from '../models/Board';
 import CellComponent from './CellComponent';
+import { Cell } from '../models/Cell';
 
 interface BoardProps {
   board: Board;
@@ -8,12 +9,31 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+  const clickCell = (cell: Cell) => cell.figure && setSelectedCell(cell);
+  const highlightCells = () => {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  };
+  const updateBoard = () => {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  };
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell])
   return (
     <div className='board'>
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
-          {row.map(cell => (
-            <CellComponent key={cell.id} cell={cell} />
+          {row.map((cell) => (
+            <CellComponent
+              key={cell.id}
+              selectedCell={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+              cell={cell}
+              clickCell={clickCell}
+            />
           ))}
         </React.Fragment>
       ))}
