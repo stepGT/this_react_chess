02c75bc6a3dc17525d1bc226a1fff174b383d98d
stepGT <stepgt@mail.ru>
2022-09-06 13:40:01 +1000

    Removed eslint-disable-next-line && ts-ignore

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -1,9 +1,7 @@
 import { Cell } from '../Cell';
 import { Colors } from '../Colors';
 import { Figure, FigureNames } from './Figure';
-// @ts-ignore
 import blackLogo from '../../assets/black-king.png';
-// @ts-ignore
 import whiteLogo from '../../assets/white-king.png';
 
 export class King extends Figure {
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -1,9 +1,7 @@
 import { Cell } from '../Cell';
 import { Colors } from '../Colors';
 import { Figure, FigureNames } from './Figure';
-// @ts-ignore
 import blackLogo from '../../assets/black-rook.png';
-// @ts-ignore
 import whiteLogo from '../../assets/white-rook.png';
 
 export class Rook extends Figure {
