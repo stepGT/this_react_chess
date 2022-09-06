72a0c11c778108b77a8eaed182786b6344df090f
stepGT <stepgt@mail.ru>
2022-09-06 13:31:33 +1000

    Added types PNG

--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -1,9 +1,7 @@
 import { Cell } from '../Cell';
 import { Colors } from '../Colors';
 import { Figure, FigureNames } from './Figure';
-// @ts-ignore
 import blackLogo from '../../assets/black-bishop.png';
-// @ts-ignore
 import whiteLogo from '../../assets/white-bishop.png';
 
 export class Bishop extends Figure {
