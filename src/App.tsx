99233071ac5c4bdd0328072d518978809caeea9d
stepGT <stepgt@mail.ru>
2022-09-14 10:51:39 +1000

    Added component Timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
 import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 import { Board } from './models/Board';
 import { Colors } from './models/Colors';
 import { Player } from './models/Player';
@@ -29,6 +30,7 @@ const App = () => {
   }, []);
   return (
     <div className="app">
+      <Timer currentPlayer={currentPlayer} restart={restart} />
       <BoardComponent
         currentPlayer={currentPlayer}
         switchPlayer={switchPlayer}
